6f5d950417831489a46931b307e5ee56ea2599fe
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2024-09-12 19:27:06 +0700

    return turtle

---
+++ turtle101.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.speed(10)
+i = 500
+while True :
+    turtle.forward(i)
+    turtle.left(120)
+    turtle.forward(i)
+    turtle.left(120)
+    turtle.forward(i)
+    turtle.left(120)
+    i /= 1.1
