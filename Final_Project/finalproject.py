9ba58eb118038c887fb07d9403e3b3d6e035b596
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2024-09-22 23:05:42 +0700

    move to bcrypt lib

--- Final_Project/finalproject.py
+++ Final_Project/finalproject.py
@@ -1,4 +1,36 @@
-# Password Manager
+import os
+import json
+import hashlib
 
-from finalprojectlib import *
+def store_password(username: str, password: str) -> None:
+    hashed_password = hash_password(password)
+    hashed_password_hex = hashed_password.hex()
+    data = {username: hashed_password_hex}
+    if os.path.exists('data.json'):
+        with open('data.json', 'r') as f:
+            existing_data = json.load(f)
+        existing_data.update(data)
+    else:
+        existing_data = data
 
+    with open('data.json', 'w') as f:
+        json.dump(existing_data, f)
+
+def hash_password(password: str) -> bytes:
+    password = bytes(password, "utf-8")
+    hashed_password = hashlib.sha384(password).digest()
+    return hashed_password
+
+def retrived_password(username: str) -> str:
+    if os.path.exists('data.json'):
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+        if username in data:
+            return data[username]
+    return None
+
+def clear_screen() -> None:
+    os.system('clear')
+
+store_password("็Harbour", "Space")
+print(retrived_password("Akarapong"))
---
+++ Final_Project/finalproject2.py
@@ -0,0 +1,15 @@
+import os
+import json
+import bcrypt
+
+def store_password(username: str, password: str) -> None:
+    pass
+
+def hash_password(password: str):
+    pass
+
+def retrived_password(username: str) -> str:
+    pass
+
+def clear_screen() -> None:
+    os.system('clear')
