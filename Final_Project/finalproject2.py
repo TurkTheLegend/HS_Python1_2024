5f52f1377edf9c696b45268d7594a2322fcb1e25
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2024-09-22 23:14:15 +0700

    salt

--- Final_Project/finalproject2.py
+++ Final_Project/finalproject2.py
@@ -3,13 +3,16 @@ import json
 import bcrypt
 
 def store_password(username: str, password: str) -> None:
+    salt = bcrypt.gensalt()
     pass
 
-def hash_password(password: str):
-    pass
+
 
 def retrived_password(username: str) -> str:
     pass
 
 def clear_screen() -> None:
     os.system('clear')
+
+salt = bcrypt.gensalt()
+print(salt)
